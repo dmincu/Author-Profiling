381e75559462ab28186f18a408b430416dce71fb
Florin Brad <florin.brad42@gmail.com>
2015-04-22 20:02:34 +0300

    0.12 Added features.py

---
+++ features.py
@@ -0,0 +1,33 @@
+import preproc
+
+# --------------- functions for computing features ----------------
+
+#id: user id
+#users: array of User instances
+#Returns a self-reference count for the user with the given id
+def get_user_self_ref_count(id, users):
+	user = None
+	for u in users:
+		if u.user_id == id:
+			user = u
+			break
+
+	self_refs = ['I', 'me', 'my']
+	count = 0
+
+	for tweet in user.documents:
+		toks = tweet.split()
+		for tok in toks:
+		    if tok in self_refs:
+		        count += 1
+
+	return count
+
+#df: users dataframe
+#users: array of User instances
+#Add the 'self_ref_count' feature to the users dataframe
+#and return the modified dataframe
+def add_self_references_count(df, users):
+    df['self_ref_count'] = df['user_id'].map(lambda id : get_user_self_ref_count(id, users))
+
+    return df
