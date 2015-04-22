c2e6ec067ac04e229145b8afdfdce07d4ad6bdb3
Diana Mincu <diana.p.mincu@gmail.com>
2015-04-22 23:44:45 +0300

    Added word extraction from users and longer than 6 word detection

--- features.py
+++ features.py
@@ -1,4 +1,13 @@
+import operator
 import preproc
+
+from gensim import corpora, models, similarities
+
+
+
+LONGWORDLEN = 6
+COUNTTOPWORDS = 20
+
 
 # --------------- functions for computing features ----------------
 
@@ -31,3 +40,61 @@ def add_self_references_count(df, users):
     df['self_ref_count'] = df['user_id'].map(lambda id : get_user_self_ref_count(id, users))
 
     return df
+
+
+def add_topics(df, users):
+	user_sentences = preproc.get_all_documents(users)
+
+	texts = user_sentences[0]
+	id2word = corpora.Dictionary(texts)
+	mm = [id2word.doc2bow(text) for text in texts]
+	lda = models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=clusters, update_every=1, chunksize=10000, passes=1)
+
+	print
+	# Prints the topics.
+	for top in lda.print_topics():
+		l = [s.split('*')[1] for s in top.split('+')]
+		toprint = ""
+		for s in l:
+			toprint += s + ", "
+		print toprint
+	print
+
+
+def add_long_words(df, users):
+	docs = preproc.get_all_documents(users)
+
+	rez = {}
+	for user, doc in docs.iteritems():
+		aux = [word.lower() for word in doc if len(word) >= LONGWORDLEN]
+		for word in aux:
+			if word in rez:
+				rez[word] += 1
+			else:
+				rez[word] = 1
+
+	sorted_rez = sorted(rez.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+	print sorted_rez
+
+	result = []
+	for i in range(COUNTTOPWORDS):
+		result.append(sorted_rez[i][0])
+
+	print result
+
+	for user, doc in docs.iteritems():
+		for elem in result:
+			if elem in doc:
+				df[user.user_id][elem] = 1
+			else:
+				df[user.user_id][elem] = 0
+
+	return df
+
+
+if __name__ == "__main__":
+	path = 'pan15-author-profiling-training-dataset-2015-03-02\\pan15-author-profiling-training-dataset-english-2015-03-02\\'
+	users = preproc.load_users(path)
+
+	#add_long_words("", users)
--- preproc.py
+++ preproc.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ET
 import os
+import re
+
 from user import User
 
 #path: path to training corpus
@@ -22,7 +24,17 @@ def load_users(path):
         
     return users
 
-def main():
-	path = 'pan15-author-profiling-training-dataset-2015-03-02\\pan15-author-profiling-training-dataset-english-2015-03-02\\'
-	users = load_users(path)
-	print users[0].get_documents()
+def get_all_documents(users):
+    docs = {}
+
+    for user in users:
+        rez = []
+        for x in user.get_documents():
+            rez = rez + re.findall(r"\w+", x.encode('utf-8'))
+        docs[user] = rez
+
+    return docs
+
+if __name__ == "__main__":
+    path = 'pan15-author-profiling-training-dataset-2015-03-02\\pan15-author-profiling-training-dataset-english-2015-03-02\\'
+    users = load_users(path)
